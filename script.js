4f04924ed423bfbd6601ec8fd79ff14707cf9dc9
maniek9101 <janczakpawelek@gmail.com>
2019-11-04 23:09:22 +0100

    u

--- script.js
+++ script.js
@@ -38,3 +38,89 @@ window.addEventListener('click',(event)=>{
 });
 
 //end
+
+const headerNavMobile = document.querySelector('.header-nav .header-nav-mobile');
+        const btnMobile = document.querySelector('.header-nav .header-nav-mobile button');
+        const nav = document.querySelector('nav')
+        const navUlLanguage = document.querySelector('nav ul li ul');
+        //navigacja !!
+        var w=window,
+        d=document,
+        e=d.documentElement,
+        g=d.getElementsByTagName('body')[0],
+        x=w.innerWidth||e.clientWidth||g.clientWidth,
+        y=w.innerHeight||e.clientHeight||g.clientHeight;
+        console.log('width: ',x,' height: ',y);
+        if(x <= 768)
+        {
+            //console.log('mobile verion');
+            headerNavMobile.setAttribute('id','fix-nav-mobile');
+            btnMobile.addEventListener('click',(e)=>{
+                //console.log('click mobile btn');
+                e.stopPropagation();
+                if(nav.getAttribute('id')=='active-nav')
+                {
+                    nav.setAttribute('id','');
+                }
+                else
+                {
+                    nav.setAttribute('id','active-nav');
+                }
+                
+            });
+            window.addEventListener('click',()=>{
+                nav.setAttribute('id','');
+            });
+            /*document.querySelector('nav ul li:nth-of-type(6)').addEventListener('click',(e)=>{
+                e.stopPropagation();
+            });*/
+        }
+        //end navigacja !!
+
+        //fix nav while scroll//
+        let navToTop ,imgInNavToTop;
+
+        window.onload = function()
+        {
+            navToTop = document.querySelector('nav').offsetTop;
+            imgInNavToTop = document.querySelector('nav img').offsetTop;
+            console.log('nav to top: ',navToTop);
+        }
+        
+        function fixNav() {            
+            
+            if(x > 768) //more then mobile 
+            {
+                if(localStorage.getItem('cookie')=='true')
+                {
+                    //nie ma cookies info 
+                    //console.log('YOU ARE IN TRUE COOKIE');
+                    if(window.scrollY > 0)
+                    {
+                        nav.setAttribute('id','fix-nav');
+                    }
+                    else
+                    {
+                        nav.setAttribute('id','');
+                    }
+                }
+                else
+                {
+                    if (window.scrollY >= navToTop) {
+                        //fix
+                        //nav.classList.add('fix-nav');
+                        //ul.style.paddingTop = 0;
+                        nav.setAttribute('id','fix-nav');
+                        //console.log('fixed');
+                    } else {
+                        //nav.classList.remove('fix-nav');
+                        nav.setAttribute('id','');
+                        //ul.style.paddingTop = '2vh';
+                        //console.log('NOOO fixed');
+                    }
+                }
+            }
+        }
+
+        window.addEventListener('scroll', fixNav);
+        //end fix nav while scroll//
